61f1bea35f0d31880af64bf2bf39c29185be0508
gacetta <78240758+gacetta@users.noreply.github.com>
2022-11-27 22:34:46 -0800

    feat: working through andrew's class

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -1,13 +1,49 @@
 const puzzleEl = document.querySelector('#puzzle');
+const guessesEl = document.querySelector('#guesses');
 const statusEl = document.querySelector('#status');
+let game1;
 
-const game1 = new Hangman('Cat in the hat', 2);
-
-puzzleEl.textContent = game1.puzzle;
-statusEl.textContent = game1.statusMessage;
+// puzzleEl.textContent = game1.puzzle;
+// statusEl.textContent = game1.statusMessage;
 
 window.addEventListener('keypress', (e) => {
   game1.makeGuess(e.key);
-  puzzleEl.textContent = game1.puzzle;
-  statusEl.textContent = game1.statusMessage;
+  render();
 })
+
+const render = () => {
+  puzzleEl.textContent = game1.puzzle;
+  guessesEl.textContent = `Guessed letters: ${game1.guessedLetters}`;
+  statusEl.textContent = game1.statusMessage;
+}
+
+const startGame = async () => {
+  const puzzle = await getPuzzle('5');
+  game1 = new Hangman(puzzle, 9);
+  render();
+}
+
+document.querySelector('#reset').addEventListener('click', startGame);
+
+startGame();
+
+// getPuzzle('5').then((puzzle) => {
+//   console.log(puzzle)
+// }, (err) => {
+//   console.log(`Error: ${err}`)
+// })
+
+// getPokemon(903).then((pokemon) => {
+//   console.log(pokemon)
+// }, (err) => {
+//   console.log(`Error: ${err}`)
+// })
+
+// getLocation().then((location) => {
+//   console.log(location);
+//   console.log('city: ', location.city);
+//   console.log('region: ', location.region);
+//   console.log('country: ', location.country);
+// }).catch((err) => {
+//   console.log(err);
+// })
---
+++ js-bootcamp/hangman/requests.js
@@ -0,0 +1,82 @@
+// const getPuzzle = (wordCount) => new Promise((resolve, reject) => {
+//   const request = new XMLHttpRequest();
+
+//   request.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//       const data = JSON.parse(e.target.responseText);
+//       console.log(data)
+//       resolve(data.puzzle)
+//     } else if (e.target.readyState === 4) {
+//       reject('An Error has taken place')
+//     }
+//   }) 
+  
+//   request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);      //details of the request
+//   request.send();       //send off the request
+// })
+// const getPuzzleOld = (wordCount) => {
+//   return fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
+//     if (response.status === 200) {
+//       return response.json();
+//     } else {
+//       throw new Error ('An Error has taken place')
+//     }
+//   }).then((data) => {
+//     return data.puzzle;
+//   })
+// }
+
+const getPuzzle = async (wordCount) => {
+  const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error('something wrong')
+  }
+}
+
+// const getPokemonOld = (name) => {
+//   return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => {
+//     if (response.status === 200) {
+//       return response.json();
+//     } else {
+//       throw new Error ('an error occurred')
+//     }
+//   }).then((data) => {
+//     console.log(data.name);
+//     return data;
+//   })
+// }
+
+// const getPokemon = async (name) => {
+//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+//   if (response.status === 200) {
+//     const pokemon = await response.json();
+//     return pokemon.name;
+//   } else {
+//     throw new Error ('an error occurred')
+//   }
+// }
+
+// const getLocation = async () => {
+//   const response = await fetch('https://ipinfo.io/json?token=26672be335f2ab');
+
+//   if (response.status === 200) {
+//     return await response.json();
+//   } else {
+//     throw new Error('something went down yo')
+//   }
+// } 
+
+// const getLocationOld = () => {
+//   return fetch('https://ipinfo.io/json?token=26672be335f2ab').then((response) => {
+//     if (response.status === 200) {
+//       return response.json();
+//     } else {
+//       throw new Error('ya fucked');
+//     }
+//   })
+// } 
